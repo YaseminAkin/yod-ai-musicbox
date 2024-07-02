90daaa1607e4bc026b3da7bfe8a78c42d8368ff3
Yasemin Akın <akinnyaseminn3@gmail.com>
2024-07-02 20:34:38 +0300

    add responsive design

--- Front/vite-project/src/pages/create-new-music-page.jsx
+++ Front/vite-project/src/pages/create-new-music-page.jsx
@@ -1,17 +1,46 @@
-function createNewMusic() {
+import { useState, useEffect } from 'react';
+
+function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowSize;
+}
+
+function CreateNewMusic() {
+
+    const size = useWindowSize();
 
     return (
         <>
             <div className="flex flex-col items-center justify-center min-h-screen bg-[#502B4E]">
-                <h1 className="text-white text-3xl font-bold mb-2 p-10 text-center">MUSICBOX</h1>
-                <div className="bg-gradient-to-b from-[#FFFFFF] to-[#BA8BB8] rounded-t-lg shadow-lg p-8 w-full min-h-screen p-60">
-                    <h2 className="text-[#1E1E1E] text-lg mb-6 text-center">Create new Musicbox</h2>
+                <h1 className="text-white text-3xl md:text-5xl font-bold mb-2 p-10 text-center">MUSICBOX</h1>
+                <div className="bg-gradient-to-b from-[#FFFFFF] to-[#BA8BB8] rounded-t-lg md:rounded-lg shadow-lg p-8 w-full md:max-w-3xl min-h-screen md:min-h-0 md:p-20 lg:p-32">
+                    <h2 className="text-[#1E1E1E] text-lg md:text-2xl mb-6 text-center">Create new Musicbox</h2>
                     <div className="flex flex-col items-center">
-                        <button className="bg-[#512C4F] hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full mb-4 w-72">
-                            Scan with Camera
+                        {size.width < 1024 && (
+                        <button className="bg-[#512C4F] hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full mb-4 w-72 md:w-80 lg:w-96">
+                            Use Camera
                         </button>
-                        <button className="bg-[#512C4F] hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full w-72">
-                            Import the files
+                        )}
+                        <button className="bg-[#512C4F] hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full w-72 md:w-80 lg:w-96">
+                            Import files
                         </button>
                     </div>
                 </div>
@@ -20,4 +49,4 @@ function createNewMusic() {
     )
 }
 
-export default createNewMusic
+export default CreateNewMusic;
